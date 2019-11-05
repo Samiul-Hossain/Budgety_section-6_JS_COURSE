bfda859d0cd214e2d66fbdd48314921359346f23
Samiul <samiulhossain1337@gmail.com>
2019-11-06 01:08:31 +0600

    video 7 done

--- app.js
+++ app.js
@@ -10,7 +10,27 @@ var budgetController = (function(){
 //UI CONTROLLER
 var UIController = (function(){
     
-    //SOME CODE
+    var DOMstrings = {
+        inputType: '.add__type',
+        inputDescription: '.add__description',
+        inputValue: '.add__value',
+        inputBtn: '.add__btn'
+    };
+    
+    
+    // function that will be used in other controller, so not private
+    return {
+        getInput: function(){
+            return {
+            type: document.querySelector(DOMstrings.inputType).value,//inc/exp
+            description: document.querySelector(DOMstrings.inputDescription).value,
+            value: document.querySelector(DOMstrings.inputValue).value
+            };
+        },
+        getDomstrings: function(){
+            return DOMstrings;
+        }
+    };
     
 })();
 
@@ -18,10 +38,12 @@ var UIController = (function(){
 //GLOBAL APP CONTROLLER
 var controller = (function(budgetCtrl,UICtrl){
     // . is a class selector. This is how we select in css, and in query. Same syntax.
+    var DOM = UICtrl.getDomstrings();
     
     var ctrlAddItem = function() {
-        //1. Get the field input data
-    
+    //1. Get the field input data
+        var input = UICtrl.getInput();
+        console.log(input);
     //2. Add the item to the budget controller
         
     //3. Add the item to the UI
@@ -29,11 +51,10 @@ var controller = (function(budgetCtrl,UICtrl){
     //4. Calculate the budget
     
     //5. Display the budget on the UI
-    console.log('it works');
     }
     
     
-    document.querySelector('.add__btn').addEventListener('click',ctrlAddItem);
+    document.querySelector(DOM.inputBtn).addEventListener('click',ctrlAddItem);
     
     document.addEventListener('keypress',function(event){
         if(event.keyCode === 13 || event.which === 13){
