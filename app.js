6f433040e7e24ff8a684b1a8ae04b066cb0aea1d
Samiul <samiulhossain1337@gmail.com>
2019-11-06 23:58:48 +0600

    video 8 done

--- app.js
+++ app.js
@@ -38,12 +38,24 @@ var UIController = (function(){
 //GLOBAL APP CONTROLLER
 var controller = (function(budgetCtrl,UICtrl){
     // . is a class selector. This is how we select in css, and in query. Same syntax.
+    var setupEventListeners = function(){
+        var DOM = UICtrl.getDomstrings();
+    document.querySelector(DOM.inputBtn).addEventListener('click',ctrlAddItem);
+    
+    document.addEventListener('keypress',function(event){
+        if(event.keyCode === 13 || event.which === 13){
+            ctrlAddItem();
+        }
+    });
+    };
+    
+    
+    
     var DOM = UICtrl.getDomstrings();
     
     var ctrlAddItem = function() {
     //1. Get the field input data
         var input = UICtrl.getInput();
-        console.log(input);
     //2. Add the item to the budget controller
         
     //3. Add the item to the UI
@@ -53,21 +65,20 @@ var controller = (function(budgetCtrl,UICtrl){
     //5. Display the budget on the UI
     }
     
-    
-    document.querySelector(DOM.inputBtn).addEventListener('click',ctrlAddItem);
-    
-    document.addEventListener('keypress',function(event){
-        if(event.keyCode === 13 || event.which === 13){
-            ctrlAddItem();
+    return {
+        init : function() {
+            console.log('Application has started.');
+            setupEventListeners();
         }
-    });
+    };
+    
     
     
 })(budgetController,UIController);
 
 
 
-
+controller.init();
 
 
 
