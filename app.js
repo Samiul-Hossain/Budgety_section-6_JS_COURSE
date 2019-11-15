daa302df67d9a3e2b3a98e0598fed67d35a960a4
Samiul <samiulhossain1337@gmail.com>
2019-11-16 03:19:09 +0600

    video 20 done

--- app.js
+++ app.js
@@ -162,6 +162,11 @@ var UIController = (function(){
             
         },
         
+        deleteListItem: function(selectorID){
+            var el = document.getElementById(selectorID);
+            el.parentNode.removeChild(el);
+        },
+        
         clearFields: function(){
             var fields, fieldsArr;
             fields = document.querySelectorAll(DOMstrings.inputDescription + ', '+ DOMstrings.inputValue);
@@ -263,8 +268,9 @@ var controller = (function(budgetCtrl,UICtrl){
             //1. delete item from the data structure
             budgetCtrl.deleteItem(type, ID);
             //2. delete the item from the UI
-            
+            UICtrl.deleteListItem(itemID);
             //3. Update and show the new budget
+            updateBudget();
         }
         
         
