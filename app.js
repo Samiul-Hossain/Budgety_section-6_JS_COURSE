65633c9e57d495710056555de54ccce5ce88e559
Samiul <samiulhossain1337@gmail.com>
2019-11-15 20:50:59 +0600

    video 19 done

--- app.js
+++ app.js
@@ -58,6 +58,23 @@ var budgetController = (function(){
             return newItem;
         },
         
+        deleteItem: function(type, id){
+            
+            var ids,index;
+            
+            ids = data.allItems[type].map(function(current){
+               return current.id; 
+            });
+            
+            index = ids.indexOf(id);
+            
+            if(index !== -1){
+                data.allItems[type].splice(index, 1);
+            }
+            
+            
+        },
+        
         calculateBudget: function(){
             
             // Calculate total income and expenses
@@ -232,19 +249,19 @@ var controller = (function(budgetCtrl,UICtrl){
     
     var ctrlDeleteItem = function(event){
       
-        var itemId,splitId,type,ID;
+        var itemID,splitID,type,ID;
         
-        itemId = event.target.parentNode.parentNode.parentNode.parentNode.id;
+        itemID = event.target.parentNode.parentNode.parentNode.parentNode.id;
         
-        if(itemId){
+        if(itemID){
 
             //inc-1
-            splitID = itemId.split('-');
+            splitID = itemID.split('-');
             type = splitID[0];
-            ID = splitId[1];
+            ID = parseInt(splitID[1]);
             
             //1. delete item from the data structure
-            
+            budgetCtrl.deleteItem(type, ID);
             //2. delete the item from the UI
             
             //3. Update and show the new budget
